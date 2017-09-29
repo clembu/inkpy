42f0c4a4738004f1e0458259cbbacbf04aaed9b3
Clément Busschaert <clement.busschaert@outlook.com>
2017-09-30 00:04:10 +0200

    Choice API (wrapper)

---
+++ inkpy/choice.py
@@ -0,0 +1,17 @@
+class Choice:
+
+    # Constructor
+    def __init__(self):
+        raise NotImplementedError
+
+    # Properties
+
+    @property
+    def text(self):
+        raise NotImplementedError
+
+    @property
+    def idx(self):
+        raise NotImplementedError
+
+    # Methods
